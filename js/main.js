7b2e5f825d631be8a8035c107401062aa23f8aa3
unknown <lskenqa@gmail.com>
2024-07-30 16:06:18 +0200

    Add Javascript code to prevent redirecting to DBMS webapp

--- js/main.js
+++ js/main.js
@@ -112,6 +112,23 @@
             }
         }
     });
+
+    window.addEventListener("load", function() {
+        const form = document.getElementById('rsvp-form');
+        form.addEventListener("submit", function(e) {
+          e.preventDefault();
+          const data = new FormData(form);
+          const action = e.target.action;
+          fetch(action, {
+            method: 'POST',
+            body: data,
+          })
+          .then(() => {
+            alert("Success!");
+          })
+        });
+      });
+      
     
 })(jQuery);
 
